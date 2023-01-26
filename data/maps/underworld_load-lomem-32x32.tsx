dcac95e24ac3229670eec0f9bd4fd09603e57358
Someone123770 <79580642+Someone123770@users.noreply.github.com>
2023-01-26 19:11:27 +0300

    Add files via upload
    bug fix

--- data/maps/underworld_load-lomem-32x32.tsx
+++ data/maps/underworld_load-lomem-32x32.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.9" tiledversion="1.9.2" name="underworld_load-lomem-32x32" tilewidth="32" tileheight="32" tilecount="64" columns="8">
- <image source="../../../../../../../../Downloads/underworld_load/underworld_load/underworld_load-lomem-32x32.png" width="256" height="256"/>
+ <image source="underworld_load-lomem-32x32.png" width="256" height="256"/>
 </tileset>
